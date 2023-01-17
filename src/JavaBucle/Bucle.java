3656b805d09ef55c0cadffac8397312eb889c99d
Yeison Andres Rojas Asencio <yrojas@lucasian.com>
2023-01-17 11:20:29 -0500

    bucle

---
+++ src/JavaBucle/Bucle.java
@@ -0,0 +1,13 @@
+package JavaBucle;
+
+public class Bucle {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"camilo", "cristian", "santiago", "juan"};
+
+        for(String nombre: nombres){
+            System.out.println(nombre);
+        }
+    }
+}
